e719422191190c31e8e19483f222b12edeaf0a7f
Henrique Alves <henrique013.a@gmail.com>
2025-04-20 12:37:20 -0300

    start project

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import '@/components/App/App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>DDD Brasil - Busca de Cidades por DDD</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
