046aaa5a0d909742d16862ad99fbd7d3e5c13439
Henrique Alves <henrique013.a@gmail.com>
2025-04-20 16:12:46 -0300

    add css vars

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,8 @@
-import styles from '@/components/App/App.module.css'
-
 function App() {
   return (
     <>
       <header>
-        <h1 className={styles.title}>DDD Brasil - Busca de Cidades por DDD</h1>
+        <h1>DDD Brasil - Busca de Cidades por DDD</h1>
       </header>
     </>
   )
