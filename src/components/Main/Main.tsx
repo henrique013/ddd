6d4cec3248f72630e330e932b3db88667c5d0ed3
Henrique Alves <henrique013.a@gmail.com>
2025-04-21 16:30:35 -0300

    add .helperText in Main component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,7 +3,12 @@ import styles from '@/components/Main/Main.module.css'
 function Main() {
   return (
     <main className={styles.main}>
-      <div className="pageContainer"></div>
+      <div className="pageContainer">
+        <div className="helperText">
+          <p className="textMuted">Digite um DDD brasileiro para ver as principais cidades</p>
+          <p className="textSmall">Exemplos: 11 (São Paulo), 21 (Rio de Janeiro), 31 (Belo Horizonte)</p>
+        </div>
+      </div>
     </main>
   )
 }
