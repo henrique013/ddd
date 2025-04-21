ff1b2be27b2f414b7d6484f150b21cfd96c0a8c5
Henrique Alves <henrique013.a@gmail.com>
2025-04-21 14:01:45 -0300

    add App.module.css

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,16 @@
+import styles from '@/components/App/App.module.css'
 import Header from '@/components/Header/Header'
 import Main from '@/components/Main/Main'
 import Footer from '@/components/Footer/Footer'
 
+// TODO: remove "light-mode" class
 function App() {
   return (
-    <>
+    <div className={`${styles.app} lightMode`}>
       <Header />
       <Main />
       <Footer />
-    </>
+    </div>
   )
 }
 
