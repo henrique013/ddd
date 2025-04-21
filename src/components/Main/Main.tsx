57f68066d794ba1bce37af454439ab5e40bb6847
Henrique Alves <henrique013.a@gmail.com>
2025-04-21 09:48:33 -0300

    add Main component

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,11 @@
+import styles from '@/components/Main/Main.module.css'
+
+function Main() {
+  return (
+    <main className={styles.main}>
+      <div className="pageContainer">Test</div>
+    </main>
+  )
+}
+
+export default Main
