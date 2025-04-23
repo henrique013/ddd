cd2076c97e5931e5c57594cf3f7446ac3bc6af53
Henrique Alves <henrique013.a@gmail.com>
2025-04-23 11:53:25 -0300

    Refactor CitiesFinderRoot component

--- src/components/CitiesFinder/CitiesFinderRoot.tsx
+++ src/components/CitiesFinder/CitiesFinderRoot.tsx
@@ -20,9 +20,15 @@ function CitiesFinderRoot() {
 
   return (
     <div className={styles.citiesFinderRoot}>
-      <h2>Busca de Cidades por DDD</h2>
-      <CitiesFinder.Form onSuccess={handleSuccess} onError={handleError} />
-      {result.cities.length > 0 && <CitiesFinder.List ddd={result.ddd} cities={result.cities} />}
+      <div className="content">
+        <h2 className="title">Busca de Cidades por DDD</h2>
+        <CitiesFinder.Form onSuccess={handleSuccess} onError={handleError} />
+        {result.cities.length > 0 && <CitiesFinder.List ddd={result.ddd} cities={result.cities} />}
+      </div>
+      <div className="helperText">
+        <p className="textMuted">Digite um DDD brasileiro para ver as principais cidades</p>
+        <p className="textSmall">Exemplos: 11 (São Paulo), 21 (Rio de Janeiro), 31 (Belo Horizonte)</p>
+      </div>
     </div>
   )
 }
--- src/components/Main/MainRoot.tsx
+++ src/components/Main/MainRoot.tsx
@@ -8,13 +8,7 @@ type Props = {
 function MainRoot({ children }: Props) {
   return (
     <main className={styles.mainRoot}>
-      <div className="pageContainer">
-        {children}
-        <div className="helperText">
-          <p className="textMuted">Digite um DDD brasileiro para ver as principais cidades</p>
-          <p className="textSmall">Exemplos: 11 (São Paulo), 21 (Rio de Janeiro), 31 (Belo Horizonte)</p>
-        </div>
-      </div>
+      <div className="pageContainer">{children}</div>
     </main>
   )
 }
