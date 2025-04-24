78714d345ac03654ee274195a29441aa83acd724
Henrique Alves <henrique013.a@gmail.com>
2025-04-24 15:59:47 -0300

    Add errors doc

--- src/errors.ts
+++ src/errors.ts
@@ -1,1 +1,5 @@
+/**
+ * UserFriendlyError is a specialized class for errors that can be directly
+ * displayed to end users, containing clear and non-technical messages.
+ */
 export class UserFriendlyError extends Error {}
