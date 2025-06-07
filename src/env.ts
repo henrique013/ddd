35d2edd624d8d52900eb2e9e368619587a436494
Henrique Alves <henrique013.a@gmail.com>
2025-06-06 21:35:20 -0300

    refactor(env): rename API_BASE_URL to VITE_API_BASE_URL for consistency

--- src/env.ts
+++ src/env.ts
@@ -7,4 +7,6 @@ const envSchema = z.object({
 /**
  * The environment variables parsed from the process.env.
  */
-export const env = envSchema.parse(process.env)
+export const env = envSchema.parse({
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
+})
